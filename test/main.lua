675343f54c72bcd3c604e0a325187b3bd4fd345b
actboy168 <actboy168@gmail.com>
2016-10-18 14:17:45 +0800

    测试

--- test/main.lua
+++ test/main.lua
@@ -1,13 +1,6 @@
-key = {
-    [1] = {1, 2, 3},
-    ['size'] = {a=1,b=2},
-}
-print(_VERSION)
 
-function test(...)
-    local n = ...
-    print(_VERSION)
-    key = 2
+if not G then
+    G = 0
 end
-test(1, "2s")
-key = 4
+G = G + 1
+print(G)
--- test/vscode/main.lua
+++ test/vscode/main.lua
@@ -1,15 +1,6 @@
-key = setmetatable({
-    [1] = {1, 2, 3},
-    ['size'] = {a=1,b=2},
-}, {k = 0})
-print(_VERSION)
 
-function test(...)
-    local n = ...
-    print(_VERSION)
-    key = 2
+if not G then
+    G = 0
 end
-key = 4
-
-test(1, "2s")
-key = 5
+G = G + 1
+print(G)
