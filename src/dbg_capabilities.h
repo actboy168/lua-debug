e8e19efbc9bb7000f32c9401ef1efb6f128a780e
actboy168 <actboy168@gmail.com>
2017-05-05 17:07:44 +0800

    统一下重复的代码

---
+++ src/dbg_capabilities.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "dbg_protocol.h"
+
+namespace vscode
+{
+	inline void capabilities(wprotocol& res, int64_t seq)
+	{
+		for (auto _ : res.Object())
+		{
+			res("type").String("response");
+			res("seq").Int64(1);
+			res("command").String("initialize");
+			res("request_seq").Int64(seq);
+			res("success").Bool(true);
+			for (auto _ : res("body").Object())
+			{
+				res("supportsConfigurationDoneRequest").Bool(true);
+				res("supportsSetVariable").Bool(true);
+				res("supportsConditionalBreakpoints").Bool(true);
+			}
+		}
+	}
+}
