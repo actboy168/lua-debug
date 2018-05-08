35cb678cf68562415f5c048b47b596fd7f37c82e
actboy168 <actboy168@gmail.com>
2018-05-08 15:19:15 +0800

    支持无launch.json测试单文件

--- publish/extension/configurationProvider.js
+++ publish/extension/configurationProvider.js
@@ -1,16 +1,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
+const path_1 = require("path");
 
 class LuaConfigurationProvider {
     /**
      * Returns an initial debug configuration based on contextual information, e.g. package.json or folder.
      */
     provideDebugConfigurations(folder, token) {
+        return [createLaunchConfig(folder)];
     }
     /**
      * Try to add all missing attributes to the debug configuration being launched.
      */
     resolveDebugConfiguration(folder, config, token) {
+        if (!config.type && !config.request && !config.name) {
+            config = createLaunchConfig(folder);
+        }
         config.type = 'lua';
         config.workspaceFolder = '${workspaceFolder}';
         if (config.request != 'attach') {
@@ -52,3 +57,29 @@ class LuaConfigurationProvider {
     }
 }
 exports.LuaConfigurationProvider = LuaConfigurationProvider;
+
+function createLaunchConfig(folder) {
+    const config = {
+        type: 'lua',
+        request: 'launch',
+        name: 'Launch'
+    };
+    let program;
+    const editor = vscode.window.activeTextEditor;
+    if (editor && editor.document.languageId === 'lua') {
+        const wf = vscode.workspace.getWorkspaceFolder(editor.document.uri);
+        if (wf === folder) {
+            program = vscode.workspace.asRelativePath(editor.document.uri);
+            if (!path_1.isAbsolute(program)) {
+                program = '${workspaceFolder}/' + program;
+            }
+        }
+    }
+    if (program) {
+        config['program'] = program;
+    }
+    else {
+        config['program'] = '${file}';
+    }
+    return config;
+}
