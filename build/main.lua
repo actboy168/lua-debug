baa6a9c06cce8913b855dc6fde5d7c6bcafd1c81
actboy168 <actboy168@gmail.com>
2018-05-09 14:05:22 +0800

    先编译再复制到publish

--- build/main.lua
+++ build/main.lua
@@ -1,3 +1,5 @@
+print 'Step 1. init'
+
 require 'filesystem'
 
 local root = fs.absolute(fs.path '../')
@@ -20,10 +22,34 @@ local function copy_directory(from, to, filter)
     end
 end
 
+print 'Step 2. remove old file'
+fs.remove_all(root / 'project' / 'bin')
+fs.remove_all(root / 'project' / 'obj' / 'Release')
+fs.remove_all(root / 'publish')
+
+print 'Step 3. compile targetcpu = x86'
+msvc:rebuild(root / 'project' / 'vscode-lua-debug.sln', 'Release', 'Win32')
+copy_directory(root / 'project' / 'bin' / 'x86', root / 'publish' / 'windows' / 'x86',
+    function (path)
+        local ext = path:extension():string():lower()
+        return (ext == '.dll') or (ext == '.exe')
+    end
+)
+
+print 'Step 4. compile targetcpu = x64'
+msvc:rebuild(root / 'project' / 'vscode-lua-debug.sln', 'Release', 'x64')
+copy_directory(root / 'project' / 'bin' / 'x64', root / 'publish' / 'windows' / 'x64',
+    function (path)
+        local ext = path:extension():string():lower()
+        return (ext == '.dll') or (ext == '.exe')
+    end
+)
+
+print 'Step 5. copy extension'
 copy_directory(root / 'extension', root / 'publish')
+
+print 'Step 6. copy crt dll'
 msvc:copy_crt_dll('x86', root / 'publish' / 'windows' / 'x86')
 msvc:copy_crt_dll('x64', root / 'publish' / 'windows' / 'x64')
-msvc:rebuild(root / 'project' / 'vscode-lua-debug.sln', 'Release', 'Win32')
-msvc:rebuild(root / 'project' / 'vscode-lua-debug.sln', 'Release', 'x64')
 
-print 'ok'
+print 'finish.'
