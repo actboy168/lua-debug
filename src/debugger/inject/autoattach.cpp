7dbd99a41cf8f2346bb9c1a7ad4077c63d064d36
actboy168 <actboy168@gmail.com>
2018-08-28 15:14:09 +0800

    整理代码

---
+++ src/debugger/inject/autoattach.cpp
@@ -0,0 +1,197 @@
+#include <debugger/inject/autoattach.h>
+#include <lua.hpp>
+#include <mutex>
+#include <stack>
+#include <set>
+#include <vector>
+#include <base/hook/fp_call.h>
+#include <base/hook/inline.h>
+
+namespace autoattach {
+	std::mutex lockLoadDll;
+	std::set<std::wstring> loadedModules;
+	HMODULE luaDll = NULL;
+	fn_attach debuggerAttach;
+	fn_detach debuggerDetach;
+	bool      attachProcess = false;
+
+	namespace lua {
+		namespace real {
+			uintptr_t lua_newstate = 0;
+			uintptr_t luaL_newstate = 0;
+			uintptr_t lua_pcallk = 0;
+			uintptr_t lua_callk = 0;
+			uintptr_t lua_close = 0;
+		}
+		namespace fake {
+			static lua_State* __cdecl lua_newstate(void* f, void* ud)
+			{
+				lua_State* L = base::c_call<lua_State*>(real::lua_newstate, f, ud);
+				debuggerAttach(L);
+				return L;
+			}
+			static lua_State* __cdecl luaL_newstate()
+			{
+				lua_State* L = base::c_call<lua_State*>(real::luaL_newstate);
+				debuggerAttach(L);
+				return L;
+			}
+			static int __cdecl lua_pcallk(lua_State *L, int nargs, int nresults, int errfunc, lua_KContext ctx, lua_KFunction k)
+			{
+				debuggerAttach(L);
+				return base::c_call<int>(real::lua_pcallk, L, nargs, nresults, errfunc, ctx, k);
+			}
+			static void __cdecl lua_callk(lua_State *L, int nargs, int nresults, lua_KContext ctx, lua_KFunction k)
+			{
+				debuggerAttach(L);
+				return base::c_call<void>(real::lua_callk, L, nargs, nresults, ctx, k);
+			}
+			static void __cdecl lua_close(lua_State* L)
+			{
+				debuggerDetach(L);
+				Sleep(1000);
+				return base::c_call<void>(real::lua_close, L);
+			}
+		}
+
+		bool hook(HMODULE m)
+		{
+			struct Hook {
+				uintptr_t& real;
+				uintptr_t fake;
+			};
+			std::vector<Hook> tasks;
+
+#define HOOK(name) do {\
+			real::##name = (uintptr_t)GetProcAddress(m, #name); \
+			if (!real::##name) return false; \
+			tasks.push_back({real::##name, (uintptr_t)fake::##name}); \
+		} while (0)
+
+			HOOK(lua_newstate);
+			HOOK(luaL_newstate);
+			HOOK(lua_close);
+			if (attachProcess) {
+				HOOK(lua_pcallk);
+				HOOK(lua_callk);
+			}
+
+			std::stack<base::hook::hook_t> rollback;
+			for (auto& task : tasks) {
+				base::hook::hook_t h;
+				if (!base::hook::install(&task.real, task.fake, &h)) {
+					while (!rollback.empty()) {
+						base::hook::uninstall(&rollback.top());
+						rollback.pop();
+					}
+					return false;
+				}
+				rollback.push(h);
+			}
+			luaDll = m;
+			return true;
+		}
+	}
+
+	static HMODULE enumerateModules(HANDLE hProcess, HMODULE hModuleLast, PIMAGE_NT_HEADERS32 pNtHeader)
+	{
+		MEMORY_BASIC_INFORMATION mbi = { 0 };
+		for (PBYTE pbLast = (PBYTE)hModuleLast + 0x10000;; pbLast = (PBYTE)mbi.BaseAddress + mbi.RegionSize) {
+			if (VirtualQueryEx(hProcess, (PVOID)pbLast, &mbi, sizeof(mbi)) <= 0) {
+				break;
+			}
+			if (((PBYTE)mbi.BaseAddress + mbi.RegionSize) < pbLast) {
+				break;
+			}
+			if ((mbi.State != MEM_COMMIT) ||
+				((mbi.Protect & 0xff) == PAGE_NOACCESS) ||
+				(mbi.Protect & PAGE_GUARD)) {
+				continue;
+			}
+			__try {
+				IMAGE_DOS_HEADER idh;
+				if (!ReadProcessMemory(hProcess, pbLast, &idh, sizeof(idh), NULL)) {
+					continue;
+				}
+				if (idh.e_magic != IMAGE_DOS_SIGNATURE || (DWORD)idh.e_lfanew > mbi.RegionSize || (DWORD)idh.e_lfanew < sizeof(idh)) {
+					continue;
+				}
+				if (!ReadProcessMemory(hProcess, pbLast + idh.e_lfanew, pNtHeader, sizeof(*pNtHeader), NULL)) {
+					continue;
+				}
+				if (pNtHeader->Signature != IMAGE_NT_SIGNATURE) {
+					continue;
+				}
+				return (HMODULE)pbLast;
+			}
+			__except (EXCEPTION_EXECUTE_HANDLER) {
+				continue;
+			}
+		}
+		return NULL;
+	}
+
+	static bool tryHookLuaDll(HMODULE hModule)
+	{
+		wchar_t moduleName[MAX_PATH];
+		GetModuleFileNameW(hModule, moduleName, MAX_PATH);
+		if (loadedModules.find(moduleName) != loadedModules.end()) {
+			return false;
+		}
+		loadedModules.insert(moduleName);
+		return lua::hook(hModule);
+	}
+
+	static bool findLuaDll()
+	{
+		std::unique_lock<std::mutex> lock(lockLoadDll);
+		HANDLE hProcess = GetCurrentProcess();
+		HMODULE hModule = NULL;
+		for (;;) {
+			IMAGE_NT_HEADERS32 inh;
+			if ((hModule = enumerateModules(hProcess, hModule, &inh)) == NULL) {
+				break;
+			}
+			if (tryHookLuaDll(hModule)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	uintptr_t realLoadLibraryExW = 0;
+	HMODULE __stdcall fakeLoadLibraryExW(LPCWSTR lpLibFileName, HANDLE hFile, DWORD dwFlags)
+	{
+		HMODULE hModule = base::std_call<HMODULE>(realLoadLibraryExW, lpLibFileName, hFile, dwFlags);
+		std::unique_lock<std::mutex> lock(lockLoadDll);
+		tryHookLuaDll(hModule);
+		return hModule;
+	}
+
+	static void waitLuaDll()
+	{
+		HMODULE hModuleKernel = GetModuleHandleW(L"kernel32.dll");
+		if (hModuleKernel)
+		{
+			realLoadLibraryExW = (uintptr_t)GetProcAddress(hModuleKernel, "LoadLibraryExW");
+			if (realLoadLibraryExW) {
+				base::hook::install(&realLoadLibraryExW, (uintptr_t)fakeLoadLibraryExW);
+			}
+		}
+	}
+
+	void initialize(fn_attach attach, fn_detach detach, bool ap)
+	{
+		debuggerAttach = attach;
+		debuggerDetach = detach;
+		attachProcess  = ap;
+		if (!findLuaDll()) {
+			waitLuaDll();
+		}
+	}
+
+	HMODULE luadll()
+	{
+		return luaDll;
+	}
+}
--- src/debugger/inject/main.cpp
+++ src/debugger/inject/main.cpp
@@ -1,244 +1,59 @@
 #include <Windows.h>
-#include <mutex>
-#include <stack>
-#include <set>
-#include <base/hook/inline.h>
-#include <base/hook/fp_call.h>
 #include <base/path/self.h>
 #include <base/util/format.h>
 #include <base/win/process_switch.h>
 #include <debugger/debugger.h>
 #include <debugger/io/namedpipe.h>
-#include <lua.hpp>
+#include <debugger/inject/autoattach.h>
 
-std::unique_ptr<vscode::debugger> Debugger;
+static std::unique_ptr<vscode::io::namedpipe> io;
+static std::unique_ptr<vscode::debugger> dbg;
+static fs::path binPath;
+static std::wstring pipeName;
+static bool isAttachProcess = false;
 
-struct DebuggerConfig {
-	static DebuggerConfig& Get() {
-		static DebuggerConfig s_instance;
-		return s_instance;
-	}
+static bool initialize() {
+	binPath = base::path::self().parent_path();
+	int pid = (int)GetCurrentProcessId();
+	pipeName = base::format(L"vscode-lua-debug-%d", pid);
+	isAttachProcess = base::win::process_switch::has(pid, L"attachprocess");
+	return true;
+}
 
-	HMODULE luaDll = 0;
-	fs::path binPath;
-	std::wstring pipeName;
-	bool isAttachProcess = false;
-	std::unique_ptr<vscode::io::namedpipe> io;
-
-	DebuggerConfig() {
-		binPath = base::path::self().parent_path();
-	}
-
-	bool initialize() {
-		int pid = (int)GetCurrentProcessId();
-		pipeName = base::format(L"vscode-lua-debug-%d", pid);
-		isAttachProcess = base::win::process_switch::has(pid, L"attachprocess");
-		return true;
-	}
-
-	bool createDebugger() {
-		if (!GetModuleHandleW(L"debugger.dll")) {
-			if (!LoadLibraryW((binPath / L"debugger.dll").c_str())) {
-				return false;
-			}
-		}
-		debugger_set_luadll(luaDll, ::GetProcAddress);
-		io.reset(new vscode::io::namedpipe());
-		if (!io->open_server(pipeName)) {
+static bool createDebugger() {
+	if (!GetModuleHandleW(L"debugger.dll")) {
+		if (!LoadLibraryW((binPath / L"debugger.dll").c_str())) {
 			return false;
 		}
-		Debugger.reset(new vscode::debugger(io.get()));
-		if (!DebuggerConfig::Get().isAttachProcess) {
-			Debugger->terminate_on_disconnect();
-		}
-		Debugger->wait_client();
-		Debugger->open_redirect(vscode::eRedirect::stdoutput);
-		Debugger->open_redirect(vscode::eRedirect::stderror);
-		return true;
 	}
-};
+	debugger_set_luadll(autoattach::luadll(), ::GetProcAddress);
+	io.reset(new vscode::io::namedpipe());
+	if (!io->open_server(pipeName)) {
+		return false;
+	}
+	dbg.reset(new vscode::debugger(io.get()));
+	if (!isAttachProcess) {
+		dbg->terminate_on_disconnect();
+	}
+	dbg->wait_client();
+	dbg->open_redirect(vscode::eRedirect::stdoutput);
+	dbg->open_redirect(vscode::eRedirect::stderror);
+	return true;
+}
 
-void DebuggerAttach(lua_State* L) {
+void debuggerAttach(lua_State* L) {
 	if (!L) return;
-	if (!Debugger) {
-		if (!DebuggerConfig::Get().createDebugger()) {
+	if (!dbg) {
+		if (!createDebugger()) {
 			return;
 		}
 	}
-	Debugger->attach_lua(L);
+	dbg->attach_lua(L);
 }
 
-void DebuggerDetach(lua_State* L) {
+void debuggerDetach(lua_State* L) {
 	if (!L) return;
-	if (Debugger) Debugger->detach_lua(L, true);
-}
-
-static HMODULE EnumerateModulesInProcess(HANDLE hProcess, HMODULE hModuleLast, PIMAGE_NT_HEADERS32 pNtHeader)
-{
-	MEMORY_BASIC_INFORMATION mbi = { 0 };
-	for (PBYTE pbLast = (PBYTE)hModuleLast + 0x10000;; pbLast = (PBYTE)mbi.BaseAddress + mbi.RegionSize) {
-		if (VirtualQueryEx(hProcess, (PVOID)pbLast, &mbi, sizeof(mbi)) <= 0) {
-			break;
-		}
-		if (((PBYTE)mbi.BaseAddress + mbi.RegionSize) < pbLast) {
-			break;
-		}
-		if ((mbi.State != MEM_COMMIT) ||
-			((mbi.Protect & 0xff) == PAGE_NOACCESS) ||
-			(mbi.Protect & PAGE_GUARD)) {
-			continue;
-		}
-		__try {
-			IMAGE_DOS_HEADER idh;
-			if (!ReadProcessMemory(hProcess, pbLast, &idh, sizeof(idh), NULL)) {
-				continue;
-			}
-			if (idh.e_magic != IMAGE_DOS_SIGNATURE || (DWORD)idh.e_lfanew > mbi.RegionSize || (DWORD)idh.e_lfanew < sizeof(idh)) {
-				continue;
-			}
-			if (!ReadProcessMemory(hProcess, pbLast + idh.e_lfanew, pNtHeader, sizeof(*pNtHeader), NULL)) {
-				continue;
-			}
-			if (pNtHeader->Signature != IMAGE_NT_SIGNATURE) {
-				continue;
-			}
-			return (HMODULE)pbLast;
-		}
-		__except (EXCEPTION_EXECUTE_HANDLER) {
-			continue;
-		}
-	}
-	return NULL;
-}
-
-namespace lua {
-	namespace real {
-		uintptr_t lua_newstate = 0;
-		uintptr_t luaL_newstate = 0;
-		uintptr_t lua_pcallk = 0;
-		uintptr_t lua_callk = 0;
-		uintptr_t lua_close = 0;
-	}
-	namespace fake {
-		static lua_State* __cdecl lua_newstate(void* f, void* ud)
-		{
-			lua_State* L = base::c_call<lua_State*>(real::lua_newstate, f, ud);
-			DebuggerAttach(L);
-			return L;
-		}
-		static lua_State* __cdecl luaL_newstate()
-		{
-			lua_State* L = base::c_call<lua_State*>(real::luaL_newstate);
-			DebuggerAttach(L);
-			return L;
-		}
-		static int __cdecl lua_pcallk(lua_State *L, int nargs, int nresults, int errfunc, lua_KContext ctx, lua_KFunction k)
-		{
-			DebuggerAttach(L);
-			return base::c_call<int>(real::lua_pcallk, L, nargs, nresults, errfunc, ctx, k);
-		}
-		static void __cdecl lua_callk(lua_State *L, int nargs, int nresults, lua_KContext ctx, lua_KFunction k)
-		{
-			DebuggerAttach(L);
-			return base::c_call<void>(real::lua_callk, L, nargs, nresults, ctx, k);
-		}
-		static void __cdecl lua_close(lua_State* L)
-		{
-			DebuggerDetach(L);
-			Sleep(1000);
-			return base::c_call<void>(real::lua_close, L);
-		}
-	}
-
-	bool hook(HMODULE m)
-	{
-		struct Hook {
-			uintptr_t& real;
-			uintptr_t fake;
-		};
-		std::vector<Hook> tasks;
-
-#define HOOK(name) do {\
-			real::##name = (uintptr_t)GetProcAddress(m, #name); \
-			if (!real::##name) return false; \
-			tasks.push_back({real::##name, (uintptr_t)fake::##name}); \
-		} while (0)
-
-		HOOK(lua_newstate);
-		HOOK(luaL_newstate);
-		HOOK(lua_close);
-		if (DebuggerConfig::Get().isAttachProcess) {
-			HOOK(lua_pcallk);
-			HOOK(lua_callk);
-		}
-
-		std::stack<base::hook::hook_t> rollback;
-		for (auto& task : tasks) {
-			base::hook::hook_t h;
-			if (!base::hook::install(&task.real, task.fake, &h)) {
-				while (!rollback.empty()) {
-					base::hook::uninstall(&rollback.top());
-					rollback.pop();
-				}
-				return false;
-			}
-			rollback.push(h);
-		}
-		DebuggerConfig::Get().luaDll = m;
-		return true;
-	}
-}
-
-std::mutex lockLoadDll;
-std::set<std::wstring> loadedModules;
-
-static bool TryHookLuaDll(HMODULE hModule)
-{
-	wchar_t moduleName[MAX_PATH];
-	GetModuleFileNameW(hModule, moduleName, MAX_PATH);
-	if (loadedModules.find(moduleName) != loadedModules.end()) {
-		return false;
-	}
-	loadedModules.insert(moduleName);
-	return lua::hook(hModule);
-}
-
-static bool FindLuaDll()
-{
-	std::unique_lock<std::mutex> lock(lockLoadDll);
-	HANDLE hProcess = GetCurrentProcess();
-	HMODULE hModule = NULL;
-	for (;;) {
-		IMAGE_NT_HEADERS32 inh;
-		if ((hModule = EnumerateModulesInProcess(hProcess, hModule, &inh)) == NULL) {
-			break;
-		}
-		if (TryHookLuaDll(hModule)) {
-			return true;
-		}
-	}
-	return false;
-}
-
-uintptr_t realLoadLibraryExW = 0;
-HMODULE __stdcall fakeLoadLibraryExW(LPCWSTR lpLibFileName, HANDLE hFile, DWORD dwFlags)
-{
-	HMODULE hModule = base::std_call<HMODULE>(realLoadLibraryExW, lpLibFileName, hFile, dwFlags);
-	std::unique_lock<std::mutex> lock(lockLoadDll);
-	TryHookLuaDll(hModule);
-	return hModule;
-}
-
-static void WaitLuaDll()
-{
-	HMODULE hModuleKernel = GetModuleHandleW(L"kernel32.dll");
-	if (hModuleKernel)
-	{
-		realLoadLibraryExW = (uintptr_t)GetProcAddress(hModuleKernel, "LoadLibraryExW");
-		if (realLoadLibraryExW) {
-			base::hook::install(&realLoadLibraryExW, (uintptr_t)fakeLoadLibraryExW);
-		}
-	}
+	if (dbg) dbg->detach_lua(L, true);
 }
 
 BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID /*pReserved*/)
@@ -246,13 +61,11 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID /*pReserved*/)
 	if (reason == DLL_PROCESS_ATTACH)
 	{
 		::DisableThreadLibraryCalls(module);
-		if (!DebuggerConfig::Get().initialize()) {
+		if (!initialize()) {
 			MessageBoxW(0, L"debuggerInject initialize failed.", L"Error!", 0);
 			return TRUE;
 		}
-		if (!FindLuaDll()) {
-			WaitLuaDll();
-		}
+		autoattach::initialize(debuggerAttach, debuggerDetach, isAttachProcess);
 	}
 	return TRUE;
 }
