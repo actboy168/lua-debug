0c5050b0be588664671321886135319f6c525be8
actboy168 <actboy168@gmail.com>
2019-01-31 21:58:12 +0800

    dll写错了

--- extension/client/proxy.lua
+++ extension/client/proxy.lua
@@ -74,8 +74,8 @@ end
 
 local function attach_process(pkg, pid)
     if not inject.injectdll(pid
-        , (WORKDIR / "windows" / "x86" / "debugger.dll"):string()
-        , (WORKDIR / "windows" / "x64" / "debugger.dll"):string()
+        , (WORKDIR / "windows" / "x86" / "debugger-inject.dll"):string()
+        , (WORKDIR / "windows" / "x64" / "debugger-inject.dll"):string()
     ) then
         return false
     end
