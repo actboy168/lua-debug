148de13c7f4dc8f68da8908b716eb39da71b5727
actboy168 <actboy168@gmail.com>
2019-04-03 15:54:03 +0800

    重定向支持macos和linux

--- include/debugger/redirect.h
+++ include/debugger/redirect.h
@@ -1,31 +1,33 @@
 #pragma once
 
-#if defined(_WIN32)
-
 #include <stddef.h>
 
 namespace vscode {
-	enum class std_fd {
-		STDIN = 0,
-		STDOUT,
-		STDERR,
-	};
+    enum class std_fd {
+        STDIN = 0,
+        STDOUT,
+        STDERR,
+    };
 
-	class redirector {
-	public:
-		redirector();
-		~redirector();
-
-		void   open(std_fd type);
-		void   close();
-		size_t peek();
-		size_t read(char* buf, size_t len);
-
-	private:
-        void*  m_rd;
-        void*  m_wr;
-		void*  m_old;
-		std_fd m_type;
-	};
-}
+    class redirector {
+    public:
+#if defined(_WIN32)
+        typedef void* handle_t;
+#else
+        typedef int   handle_t;
 #endif
+    public:
+        redirector();
+        ~redirector();
+        void     open(std_fd type);
+        void     close();
+        size_t   read(char* buf, size_t len);
+#if defined(_WIN32)
+        size_t   peek();
+#endif
+    private:
+        handle_t m_pipe[2];
+        handle_t m_old;
+        std_fd   m_type;
+    };
+}
