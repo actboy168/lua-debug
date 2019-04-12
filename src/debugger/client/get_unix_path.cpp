84388c7c3b0af10f8309b5cc52d1341fe94673c6
actboy168 <actboy168@gmail.com>
2019-04-12 13:51:23 +0800

    路径又算出了

--- src/debugger/client/get_unix_path.cpp
+++ src/debugger/client/get_unix_path.cpp
@@ -4,7 +4,7 @@
 #include <filesystem>
 
 std::string get_unix_path(int pid) {
-	auto path = bee::path_helper::dll_path().value().parent_path().parent_path() / "tmp";
+	auto path = bee::path_helper::dll_path().value().parent_path().parent_path().parent_path() / "tmp";
 	std::filesystem::create_directories(path);
 	return bee::w2u((path / bee::format("pid_%d.tmp", pid)).wstring());
 }
