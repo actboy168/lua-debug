3a74dc8a66f0874a6555bab2c750eccdf52c22e4
actboy168 <actboy168@gmail.com>
2019-04-16 17:44:25 +0800

    测试

--- test/undump.lua
+++ test/undump.lua
@@ -3,8 +3,9 @@
 -- publish\runtime\win32\lua53\lua.exe test\undump.lua
 -- publish\runtime\win64\lua54\lua.exe test\undump.lua
 
-package.path = "extension/script/backend/?.lua"
-local undump = require "undump"
+package.path = "extension/script/?.lua"
+local undump = require "backend.undump"
+local parser = require "backend.parser"
 
 local function getproto(f)
     local cl = undump(string.dump(f))
@@ -13,5 +14,10 @@ end
 
 getproto(function()end)
 getproto(loadfile "extension/script/backend/undump.lua")
+
+local src = {}
+parser(src, loadfile "extension/script/frontend/mainloop.lua")
+assert(src.activelines[30] == false)
+assert(src.activelines[32] == true)
 
 print "ok"
