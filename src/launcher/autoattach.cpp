7f0e7af2bc4c369492f64cabc376d5638b3377fc
actboy168 <actboy168@gmail.com>
2019-06-24 15:20:17 +0800

    移除没用的代码

--- src/launcher/autoattach.cpp
+++ src/launcher/autoattach.cpp
@@ -50,17 +50,11 @@ namespace autoattach {
 
 	namespace lua {
 		namespace real {
-			uintptr_t lua_newstate = 0;
 			uintptr_t luaL_openlibs = 0;
 			uintptr_t lua_close = 0;
 			uintptr_t lua_settop = 0;
 		}
 		namespace fake {
-			static lua_State* __cdecl lua_newstate(void* f, void* ud) {
-				lua_State* L = base::c_call<lua_State*>(real::lua_newstate, f, ud);
-				debuggerAttach(L);
-				return L;
-			}
 			static void __cdecl luaL_openlibs(lua_State* L) {
 				base::c_call<void>(real::luaL_openlibs, L);
 				debuggerAttach(L);
@@ -88,7 +82,6 @@ namespace autoattach {
 			tasks.push_back({real::##name, (uintptr_t)fake::##name}); \
 		} while (0)
 
-			//HOOK(lua_newstate);
 			HOOK(luaL_openlibs);
 			HOOK(lua_close);
 			if (attachProcess) {
@@ -205,7 +198,6 @@ namespace autoattach {
 		if (lua::real::##api && strcmp(name, #api) == 0) { \
 			return (FARPROC)lua::real::##api; \
 		}
-		//FIND(lua_newstate)
 		FIND(luaL_openlibs)
 		FIND(lua_close)
 		FIND(lua_settop)
