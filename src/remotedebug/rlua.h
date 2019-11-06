23b6a678a330c3f4297c122caf3bd31f713c1be8
actboy168 <actboy168@gmail.com>
2019-11-06 11:27:33 +0800

    修正一个错误

--- src/remotedebug/rlua.h
+++ src/remotedebug/rlua.h
@@ -110,6 +110,7 @@ void (rlua_settop) (rlua_State *L, int idx);
 void (rlua_rotate) (rlua_State *L, int idx, int n);
 void (rlua_copy) (rlua_State *L, int fromidx, int toidx);
 int  (rlua_checkstack) (rlua_State *L, int n);
+void (rluaL_checkstack) (rlua_State *L, int sz, const char *msg);
 
 void  (rluaL_buffinit) (rlua_State *L, rluaL_Buffer *B);
 char *(rluaL_prepbuffsize) (rluaL_Buffer *B, size_t sz);
