61d66fc83b729f889c6ed3294d9355d3635b53ad
actboy168 <actboy168@gmail.com>
2019-12-03 14:37:52 +0800

    给每个线程都发一遍

--- src/remotedebug/bee/rdebug_utility.cpp
+++ src/remotedebug/bee/rdebug_utility.cpp
@@ -33,25 +33,18 @@ namespace rdebug_utility {
 
     
 #if defined(_WIN32)
-    static int getThreadId(int pid) {
+    static void closeWindow() {
         HANDLE h = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
         if (h != INVALID_HANDLE_VALUE) {
             THREADENTRY32 te;
             te.dwSize = sizeof(te);
             for (BOOL ok = Thread32First(h, &te); ok; ok = Thread32Next(h, &te)) {
-                if (te.th32OwnerProcessID == pid) {
-                    CloseHandle(h);
-                    return te.th32ThreadID;
+                if (te.th32OwnerProcessID == GetCurrentProcessId()) {
+                    PostThreadMessageW(te.th32ThreadID, WM_QUIT, 0, 0);
                 }
             }
         }
         CloseHandle(h);
-        return 0;
-    }
-    static void closeWindow() {
-        int tid = getThreadId(GetCurrentProcessId());
-        PostThreadMessageW(tid, WM_CLOSE, 0, 0);
-        PostThreadMessageW(tid, WM_QUIT, 0, 0);
     }
     bool isConsoleExe(const wchar_t* exe) {
         HANDLE hExe = CreateFileW(exe, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
