2c49b3f096a46d487cbfdc37b6c4def34ea4f534
actboy168 <actboy168@gmail.com>
2020-03-13 15:02:31 +0800

    修正一个小问题

--- make/install_runtime.lua
+++ make/install_runtime.lua
@@ -9,18 +9,24 @@ if platform ~= 'msvc' or arch == 'x86' then
     local binplat = platform ~= 'msvc' and platform or 'win'
     local output = CWD / 'publish' / 'bin' / binplat
     local bindir = CWD / 'build' / platform / 'bin' / arch
-    local bootstrap = CWD / "3rd" / "bee.lua" / "bootstrap"
     fs.create_directories(output)
-    fs.copy_file(bootstrap / "main.lua",      CWD / 'build' / "main.lua",   true)
-    fs.copy_file(bindir / ('bee'..dll),       CWD / 'build' / ('bee'..dll), true)
-    fs.copy_file(bindir / ('bootstrap'..exe), CWD / 'build' / ('lua'..exe), true)
     fs.copy_file(bindir / ('bee'..dll),       output / ('bee'..dll),        true)
     fs.copy_file(bindir / ('lua'..exe),       output / ('lua-debug'..exe),  true)
     fs.copy_file(bindir / ('inject'..dll),    output / ('inject'..dll),     true)
     if platform == 'msvc' then
-        fs.copy_file(bindir / 'lua54.dll',    CWD / 'build' / 'lua54.dll',  true)
         fs.copy_file(bindir / 'lua54.dll',    output / 'lua54.dll',         true)
         require 'msvc'.copy_crtdll(arch, output)
+    end
+end
+
+do
+    local bindir = CWD / 'build' / platform / 'bin' / arch
+    local bootstrap = CWD / "3rd" / "bee.lua" / "bootstrap"
+    fs.copy_file(bootstrap / "main.lua",      CWD / 'build' / "main.lua",   true)
+    fs.copy_file(bindir / ('bee'..dll),       CWD / 'build' / ('bee'..dll), true)
+    fs.copy_file(bindir / ('bootstrap'..exe), CWD / 'build' / ('lua'..exe), true)
+    if platform == 'msvc' then
+        fs.copy_file(bindir / 'lua54.dll',    CWD / 'build' / 'lua54.dll',  true)
     end
 end
 
