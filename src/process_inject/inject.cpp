1210046fbb500a605886615c74cefb187715672c
actboy168 <actboy168@gmail.com>
2020-06-19 12:49:18 +0800

    漏了一处

--- src/process_inject/inject.cpp
+++ src/process_inject/inject.cpp
@@ -14,18 +14,18 @@ static int injectdll(lua_State* L) {
     }
     if (lua_type(L, 1) == LUA_TNUMBER) {
         DWORD pid = (DWORD)luaL_checkinteger(L, 1);
-        bool ok = injectdll(pid, bee::lua::to_string(L, 2), bee::lua::to_string(L, 3), entry);
+        bool ok = injectdll(pid, bee::lua::checkstring(L, 2), bee::lua::checkstring(L, 3), entry);
         lua_pushboolean(L, ok);
         return 1;
     }
     auto& self = *(bee::subprocess::process*)getObject(L, 1, "subprocess");
-    bool ok = injectdll(self.info(), bee::lua::to_string(L, 2), bee::lua::to_string(L, 3), entry);
+    bool ok = injectdll(self.info(), bee::lua::checkstring(L, 2), bee::lua::checkstring(L, 3), entry);
     lua_pushboolean(L, ok);
     return 1;
 }
 
 static int query_process(lua_State* L) {
-	std::wstring name = bee::lua::to_string(L, 1);
+	std::wstring name = bee::lua::checkstring(L, 1);
 	std::transform(name.begin(), name.end(), name.begin(), ::tolower);
     lua_newtable(L);
 	query_process([&](const SYSTEM_PROCESS_INFORMATION* info)->bool {
