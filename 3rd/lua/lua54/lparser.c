d90214ea0dd8c45d6ba4e31fc601228636ab7b8a
actboy168 <actboy168@gmail.com>
2020-07-09 10:29:12 +0800

    更新lua

--- 3rd/lua/lua54/lparser.c
+++ 3rd/lua/lua54/lparser.c
@@ -1851,10 +1851,12 @@ static void retstat (LexState *ls) {
     nret = explist(ls, &e);  /* optional return values */
     if (hasmultret(e.k)) {
       luaK_setmultret(fs, &e);
-      //if (e.k == VCALL && nret == 1 && !fs->bl->insidetbc) {  /* tail call? */
-      //  SET_OPCODE(getinstruction(fs,&e), OP_TAILCALL);
-      //  lua_assert(GETARG_A(getinstruction(fs,&e)) == luaY_nvarstack(fs));
-      //}
+#if 0
+      if (e.k == VCALL && nret == 1 && !fs->bl->insidetbc) {  /* tail call? */
+        SET_OPCODE(getinstruction(fs,&e), OP_TAILCALL);
+        lua_assert(GETARG_A(getinstruction(fs,&e)) == luaY_nvarstack(fs));
+      }
+#endif
       nret = LUA_MULTRET;  /* return all values */
     }
     else {
