5be1cb56550b0a4bbda247f61a6eed6312720895
actboy168 <actboy168@gmail.com>
2020-07-09 16:57:52 +0800

    修正编译错误

--- make.lua
+++ make.lua
@@ -224,7 +224,6 @@ else
             },
             sources = {
                 "src/process_inject/injectdll.cpp",
-                "src/process_inject/query_process.cpp",
                 "src/process_inject/inject.cpp",
                 lm.arch == "x86" and "3rd/wow64ext/src/wow64ext.cpp",
             },
