9d11d90d0f353c5c831782499047783fb4dc5f5d
actboy168 <actboy168@gmail.com>
2020-07-13 11:09:53 +0800

    更新测试

--- test/undump.lua
+++ test/undump.lua
@@ -7,17 +7,22 @@ package.path = "extension/script/?.lua"
 local undump = require "backend.worker.undump"
 local parser = require "backend.worker.parser"
 
+package.loaded["backend.worker.evaluate"] = {
+    dump = function(content)
+        return true, string.dump(content)
+    end,
+}
+
 local function getproto(f)
     local cl = undump(string.dump(f))
     return cl.f
 end
 
 getproto(function()end)
-getproto(loadfile "extension/script/backend/undump.lua")
+getproto(assert(loadfile "extension/script/backend/worker/undump.lua"))
 
-local src = {}
-parser(src, loadfile "extension/script/frontend/mainloop.lua")
-assert(src.activelines[30] == false)
-assert(src.activelines[32] == true)
+local si = parser(assert(loadfile "extension/script/frontend/main.lua"))
+assert(si.activelines[25] == false)
+assert(si.activelines[27] == true)
 
 print "ok"
