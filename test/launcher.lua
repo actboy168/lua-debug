c07631b8157a07fef89bc8148fbb780a6a4ac2b9
actboy168 <actboy168@gmail.com>
2020-07-14 16:45:46 +0800

    更新test

--- test/launcher.lua
+++ test/launcher.lua
@@ -1,10 +1,5 @@
-package.cpath = "build/linux/bin/runtime/x64/lua54/?.so"
-package.path = "extension/script/?.lua"
-
-local rdebug = require "remotedebug"
-local dbg = (loadfile "extension/script/start_debug.lua")(rdebug, "test", "/../"..package.path, "/../"..package.cpath)
-dbg:io("listen:127.0.0.1:4278")
-dbg:wait()
-dbg:start()
+loadfile "publish/script/debugger.lua" "publish"
+:start "127.0.0.1:4278"
+:wait()
 
 print "ok"
