aa7e58f06c79d5f93ebcce8245c9c2fae0138f4a
actboy168 <actboy168@gmail.com>
2021-02-18 15:29:56 +0800

    Fixes #118

--- extension/script/backend/worker/eval/repl.lua
+++ extension/script/backend/worker/eval/repl.lua
@@ -12,39 +12,41 @@ if _VERSION == "Lua 5.1" then
 end
 
 local f = assert(debug.getinfo(level,"f").func, "can't find function")
-local args = {}
+local args_name = {}
+local args_value = {}
 local locals = {}
-local local_id = {}
-local local_value = {}
-local upvalue_id = {}
-local i = 1
-while true do
-	local name, value = debug.getlocal(level, i)
-	if name == nil then
-		break
+do
+	local i = 1
+	while true do
+		local name, value = debug.getupvalue(f, i)
+		if name == nil then
+			break
+		end
+		if #name > 0 then
+			args_name[#args_name+1] = name
+			args_value[name] = value
+		end
+		i = i + 1
 	end
-	if name:byte() ~= 40 then	-- '('
-		args[#args+1] = name
-		locals[#locals+1] = ("[%d]=%s,"):format(i,name)
-		local_id[name] = i
-		local_value[name] = value
-	end
-	i = i + 1
 end
-local i = 1
-while true do
-	local name = debug.getupvalue(f, i)
-	if name == nil then
-		break
+do
+	local i = 1
+	while true do
+		local name, value = debug.getlocal(level, i)
+		if name == nil then
+			break
+		end
+		if name:byte() ~= 40 then	-- '('
+			args_name[#args_name+1] = name
+			args_value[name] = value
+			locals[#locals+1] = ("[%d]={%s},"):format(i,name)
+		end
+		i = i + 1
 	end
-	if #name > 0 then
-		upvalue_id[name] = i
-		args[#args+1] = name
-	end
-	i = i + 1
 end
+
 local full_source
-if #args > 0 then
+if #args_name > 0 then
 	full_source = ([[
 local $ARGS
 return function(...)
@@ -54,7 +56,7 @@ function()
 return {$LOCALS}
 end
 ]]):gsub("%$(%w+)", {
-	ARGS = table.concat(args, ","),
+	ARGS = table.concat(args_name, ","),
 	SOURCE = source,
 	LOCALS = table.concat(locals),
 })
@@ -73,41 +75,38 @@ end
 end
 
 local func, update = assert(load(full_source, '=(EVAL)'))()
-local i = 1
-while true do
-	local name = debug.getupvalue(func, i)
-	if name == nil then
-		break
-	end
-	local uvid = upvalue_id[name]
-	if uvid then
-		debug.upvaluejoin(func, i, f, uvid)
-	end
-	if local_id[name] then
-		debug.setupvalue(func, i, local_value[name])
-	end
-	i = i + 1
-end
-local rets
-local vararg, v = debug.getlocal(level, -1)
-if vararg then
-	local vargs = { v }
-	local i = 2
+do
+	local i = 1
 	while true do
-		vararg, v = debug.getlocal(level, -i)
-		if vararg then
-			vargs[i] = v
-		else
+		local name = debug.getupvalue(func, i)
+		if name == nil then
 			break
 		end
-		i=i+1
+		debug.setupvalue(func, i, args_value[name])
+		i = i + 1
 	end
-	rets = table.pack(func(table.unpack(vargs)))
-else
-	rets = table.pack(func())
 end
-local needupdate = update()
-for k,v in pairs(needupdate) do
-	debug.setlocal(level,k,v)
+local rets
+do
+	local vararg, v = debug.getlocal(level, -1)
+	if vararg then
+		local vargs = { v }
+		local i = 2
+		while true do
+			vararg, v = debug.getlocal(level, -i)
+			if vararg then
+				vargs[i] = v
+			else
+				break
+			end
+			i = i + 1
+		end
+		rets = table.pack(func(table.unpack(vargs)))
+	else
+		rets = table.pack(func())
+	end
+end
+for k,v in pairs(update()) do
+	debug.setlocal(level,k,v[1])
 end
 return table.unpack(rets)
