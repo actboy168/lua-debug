c420ca3d123884dd7b4705550a98430557c1ce8a
actboy168 <actboy168@gmail.com>
2021-04-06 16:43:26 +0800

    支持macos arm64

--- src/remotedebug/thunk/thunk.cpp
+++ src/remotedebug/thunk/thunk.cpp
@@ -8,7 +8,11 @@
 #		include "thunk_windows_i386.inl"
 #	endif
 #elif defined(__linux__) || defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
-#	include "thunk_linux.inl"
+#	if defined(__ia64__)
+#		include "thunk_linux.inl"
+#else
+#		include "thunk_other.inl"
+#	endif
 #else
 #	include "thunk_other.inl"
 #endif
--- src/remotedebug/thunk/thunk.h
+++ src/remotedebug/thunk/thunk.h
@@ -7,7 +7,15 @@ struct thunk;
 thunk* thunk_create_hook(intptr_t dbg, intptr_t hook);
 thunk* thunk_create_allocf(intptr_t dbg, intptr_t allocf);
 
-#if defined(_WIN32) || defined(__linux__) || defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
+#if defined(_WIN32)
+#	define THUNK_JIT 1
+#elif defined(__linux__) || defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
+#	if defined(__ia64__)
+#		define THUNK_JIT 1
+#	endif
+#endif
+
+#if defined(THUNK_JIT)
 struct thunk {
 	void*  data = 0;
 	size_t size = 0;
