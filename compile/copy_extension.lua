c318fcc5e7aa882075c004d3fe43d7dc7b5e2ca7
actboy168 <actboy168@gmail.com>
2021-09-11 23:12:40 +0800

    整理代码

--- compile/copy_extension.lua
+++ compile/copy_extension.lua
@@ -3,24 +3,18 @@ local root = fs.absolute(fs.path '.')
 local outputDir = root / 'publish'
 local OVERWRITE <const> = fs.copy_options.overwrite_existing
 
-local function copy_directory(from, to, filter)
+local function copy_directory(from, to)
     fs.create_directories(to)
     for fromfile in from:list_directory() do
         if fs.is_directory(fromfile) then
-            copy_directory(fromfile, to / fromfile:filename(), filter)
+            copy_directory(fromfile, to / fromfile:filename())
         else
-            if (not filter) or filter(fromfile) then
-                fs.copy_file(fromfile, to / fromfile:filename(), OVERWRITE)
-            end
+            fs.copy_file(fromfile, to / fromfile:filename(), OVERWRITE)
         end
     end
 end
 
-copy_directory(root / 'extension', outputDir,
-function (path)
-    local ext = path:extension():string():lower()
-    return (ext ~= '.dll') and (ext ~= '.exe')
-end)
+copy_directory(root / 'extension', outputDir)
 
 fs.copy_file(root / "LICENSE",   outputDir / "LICENSE",   OVERWRITE)
 fs.copy_file(root / "README.md", outputDir / "README.md", OVERWRITE)
