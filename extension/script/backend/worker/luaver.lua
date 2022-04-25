b508f182314e1dc06c4f6f2c4b13a8cbf2e5ac7b
actboy168 <actboy168@gmail.com>
2022-04-25 14:45:37 +0800

    做个判空

--- extension/script/backend/worker/luaver.lua
+++ extension/script/backend/worker/luaver.lua
@@ -5,6 +5,10 @@ local m = {}
 function m.init()
     local version = rdebug.fieldv(rdebug._G, "_VERSION")
     local ver = 0
+    if type(version) ~= "string" then
+        m.LUAVERSION = 54
+        return
+    end
     for n in version:gmatch "%d" do
         ver = ver * 10 + (math.tointeger(n) or 0)
     end
