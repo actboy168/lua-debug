4778a6686f4bb36b72aad0f3f695b7497e487b35
actboy168 <actboy168@gmail.com>
2022-06-01 10:31:01 +0800

    使用msvc_copydll

--- compile/windows/copy_vcredist.lua
+++
@@ -1,5 +0,0 @@
-local fs = require 'bee.filesystem'
-local CWD = fs.current_path()
-local msvc = require 'msvc'
-msvc.copy_vcrt("x64", CWD / 'publish' / 'vcredist' / "win32-x64")
-msvc.copy_vcrt("x86", CWD / 'publish' / 'vcredist' / "win32-ia32")
