3d572cf5fa0058119121662c59508aeac0cb4dba
actboy168 <actboy168@gmail.com>
2022-12-09 11:34:21 +0800

    1.60.3

--- compile/common/package_json.lua
+++ compile/common/package_json.lua
@@ -5,7 +5,7 @@ local OS, ARCH = platform:match "^([^-]+)-([^-]+)$"
 
 local json = {
     name = "lua-debug",
-    version = "1.60.2",
+    version = "1.60.3",
     publisher = "actboy168",
     displayName = "Lua Debug",
     description = "VSCode debugger extension for Lua",
