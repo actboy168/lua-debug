df8bafd9d72329f6e25c703707d5d4d0b170b3d1
fesily <fesil@foxmail.com>
2023-02-10 16:10:01 +0800

    fix windows build error

--- src/launcher/autoattach.h
+++ src/launcher/autoattach.h
@@ -1,11 +1,16 @@
 #pragma once
 #ifdef _WIN32
 #include <windows.h>
-#endif
+#else
 #include "attach_args.h"
+#endif
 
 namespace autoattach {
+#ifdef _WIN32
+	typedef void(*fn_attach)(lua_State* L);
+#else
 	typedef void(*fn_attach)(lua_State* L, attach_args* args);
+#endif
 	void    initialize(fn_attach attach, bool ap);
 #ifdef _WIN32
 	FARPROC luaapi(const char* name);
