248fc04512aa0e65f87c262b50befe5f7fd3084e
actboy168 <actboy168@gmail.com>
2023-02-17 17:30:57 +0800

    update bee

--- compile/common/runtime.lua
+++ compile/common/runtime.lua
@@ -188,7 +188,6 @@ for _, luaver in ipairs {"lua51","lua52","lua53","lua54","lua-latest","luajit"} 
             },
         },
         macos = {
-            sources = "3rd/bee.lua/bee/utility/path_helper.mm",
             frameworks = "Foundation"
         },
         linux = {
