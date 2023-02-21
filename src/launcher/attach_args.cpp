b0361d6ab776f77a7b1d75701195ba4d09db35a8
fesily <fesil@foxmail.com>
2023-02-21 13:02:41 +0800

    fix rename error

--- src/launcher/attach_args.cpp
+++ src/launcher/attach_args.cpp
@@ -4,17 +4,17 @@
 namespace autoattach {
     bool attach_args::get_symbols(void* p) {
 #define str(s) #s
-#define SymbolResolver_lua_(name, prefix) name = (name##_t)resolver->getsymbol(str(prefix##_##name));
-#define SymbolResolver_lua(name) SymbolResolver_lua_(name, lua);
-#define SymbolResolver_luaL(name) SymbolResolver_lua_(name, luaL);
+#define SymbolResolver_lua_(name, prefix) name = (name##_t)resolver->getsymbol(str(prefix##_##name))
+#define SymbolResolver_lua(name) SymbolResolver_lua_(name, lua)
+#define SymbolResolver_luaL(name) SymbolResolver_lua_(name, luaL)
 #define SymbolResolverWithCheck_lua(name) SymbolResolver_lua(name); if (!name) return false;
 		auto resolver = static_cast<symbol_resolver::interface*>(p);
         SymbolResolver_luaL(loadbuffer);
         SymbolResolver_luaL(loadbufferx);
 		if (!(loadbufferx || loadbuffer))
             return false;
-        SymbolResolverWithCheck(pushlstring);
-        SymbolResolverWithCheck(settop);
+        SymbolResolverWithCheck_lua(pushlstring);
+        SymbolResolverWithCheck_lua(settop);
         SymbolResolver_lua(pcallk);
         SymbolResolver_lua(pcall);
         if (!(pcallk || pcall))
