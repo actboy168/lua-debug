12bdffc1aa8639baa2f663bd43c07874aebc6b0a
fesily <fesil@foxmail.com>
2023-02-24 21:21:21 +0800

    delete signature resolver

--- src/launcher/symbol_resolver/symbol_resolver.cpp
+++ src/launcher/symbol_resolver/symbol_resolver.cpp
@@ -9,18 +9,11 @@
 #include "../common.hpp"
 #include "symbol_resolver.h"
 #include "../utility/string_helper.hpp"
-#include "signatures.hpp"
 
 namespace autoattach::symbol_resolver {
     std::unique_ptr<interface> create_gum_resolver(const RuntimeModule &module);
 
-    std::unique_ptr<interface> create_signature_resolver(const RuntimeModule &module, signatures&& data);
-
     std::unique_ptr<interface> symbol_resolver_factory_create(const RuntimeModule &module) {
-        const char *signature = getenv("LUA_DEBUG_SIGNATURE");
-        if (signature) {
-            return create_signature_resolver(module, signatures(signature));
-        }
         return create_gum_resolver(module);
     }
 
