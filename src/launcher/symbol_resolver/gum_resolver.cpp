33a57840b3aeecd9d87f0a90886ce53331e7ac9a
fesily <fesil@foxmail.com>
2023-02-28 09:58:32 +0800

    better getsymbol

--- src/launcher/symbol_resolver/gum_resolver.cpp
+++ src/launcher/symbol_resolver/gum_resolver.cpp
@@ -4,14 +4,17 @@
 
 namespace autoattach::symbol_resolver {
     struct gum_resolver : interface {
-		gum_resolver(const RuntimeModule& module):image_name{module.path}{}
+		gum_resolver(const RuntimeModule& module):image_name{module.name}{}
         ~gum_resolver() override = default;
 
         const char *image_name;
 
         void *getsymbol(const char *name) const override {
 			auto keys = strings::spilt_string(name, '.');
-            auto ptr = Gum::Process::module_find_symbol_by_name(image_name, keys.back().data());
+            auto key = keys.back().data();
+            auto ptr = Gum::Process::module_find_export_by_name(image_name, key);
+            if (!ptr)
+                ptr = Gum::Process::module_find_symbol_by_name(image_name, key);
             LOG(std::format("gum resolver symbol {}:{}", name, ptr).c_str());
             return ptr;
         }
