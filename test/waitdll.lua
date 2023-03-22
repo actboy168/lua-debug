a12a46f1f1c83d420534f548657f00aa2ca38b9c
fesily <fesil@foxmail.com>
2023-03-22 16:32:03 +0800

    fix test

--- test/waitdll.lua
+++ test/waitdll.lua
@@ -4,7 +4,7 @@ local sp = require "bee.subprocess"
 
 assert(platform.os == "macos")
 local runtime_platform = "darwin-"..(platform.Arch == "x86_64" and "x64" or platform.Arch)
-local lua_path = (fs.path("publish/runtime/") / runtime_platform / "lua51" / "lua51.so"):string()
+local lua_path = (fs.path("publish/runtime/") / runtime_platform / "lua51" / "lua"):string()
 local launcher_path = "publish/bin/launcher.so"
 
 local executable = fs.path("build") / runtime_platform / "debug" / "bin" / "testwaitdll"
