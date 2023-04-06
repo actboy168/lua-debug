6eb73dba90982968e65a97dae6f09bf5933cf513
fesily <fesil@foxmail.com>
2023-04-06 16:10:57 +0800

    fix windows

--- src/launcher/autoattach/lua_module.cpp
+++ src/launcher/autoattach/lua_module.cpp
@@ -70,7 +70,11 @@ namespace luadebug::autoattach {
         if (!Gum::Process::module_load(path.c_str(), &error)) {
             log::fatal("load debugger [{}] failed: {}", path, error);
         }
+#ifdef _WIN32
+        _putenv("LUA_DEBUG_CORE", std::format("LUA_DEBUG_CORE={}", path).c_str());
+#else
         setenv("LUA_DEBUG_CORE", path.c_str(), true);
+#endif
         return true;
     }
 
