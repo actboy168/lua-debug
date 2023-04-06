ee86f6379ee83bfad1dba0990fe19148d80ddf2f
actboy168 <actboy168@gmail.com>
2023-04-06 11:13:52 +0800

    fixes bug

--- src/launcher/autoattach/lua_module.h
+++ src/launcher/autoattach/lua_module.h
@@ -25,6 +25,30 @@ namespace luadebug::autoattach {
         work_mode mode;
         struct watchdog* watchdog = nullptr;
 
+        lua_module(work_mode mode)
+            : mode(mode) {}
+        lua_module(const lua_module&)            = delete;
+        lua_module& operator=(const lua_module&) = delete;
+
+        lua_module(lua_module&& rhs) {
+            swap(std::forward<lua_module>(rhs));
+        };
+        lua_module& operator=(lua_module&& rhs) {
+            swap(std::forward<lua_module>(rhs));
+            return *this;
+        };
+
+        void swap(lua_module&& rhs) {
+            std::swap(path, rhs.path);
+            std::swap(name, rhs.name);
+            std::swap(memory_address, rhs.memory_address);
+            std::swap(memory_size, rhs.memory_size);
+            std::swap(version, rhs.version);
+            std::swap(resolver, rhs.resolver);
+            std::swap(mode, rhs.mode);
+            std::swap(watchdog, rhs.watchdog);
+        }
+
         bool initialize();
         ~lua_module();
     };
