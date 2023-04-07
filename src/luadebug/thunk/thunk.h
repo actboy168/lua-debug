062e6f87ce047a12120a101e7f5c7587039979fa
fesily <fesil@foxmail.com>
2023-04-07 15:33:07 +0800

    disable ios

--- src/luadebug/thunk/thunk.h
+++ src/luadebug/thunk/thunk.h
@@ -10,8 +10,18 @@ thunk* thunk_create_allocf(intptr_t dbg, intptr_t allocf);
 #if defined(_WIN32)
 #    define THUNK_JIT 1
 #else
-#    if defined(__ia64__) || defined(__aarch64__)
+#    if defined(__ia64__)
 #        define THUNK_JIT 1
+#    elif defined(__aarch64__)
+// ios disable jit
+#        if defined(__APPLE__)
+#            include <TargetConditionals.h>
+#            if !TARGET_OS_IPHONE
+#                define THUNK_JIT 1
+#            endif
+#        else
+#            define THUNK_JIT 1
+#        endif
 #    endif
 #endif
 
