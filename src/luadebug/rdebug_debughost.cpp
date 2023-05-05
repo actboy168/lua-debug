6c715a1c456c147417a206cba77cd141874af805
fesily <fesil@foxmail.com>
2023-05-05 14:20:11 +0800

    fix lua51 user use luadebug module

--- src/luadebug/rdebug_debughost.cpp
+++ src/luadebug/rdebug_debughost.cpp
@@ -186,8 +186,23 @@ namespace luadebug::debughost {
 #endif
             { NULL, NULL },
         };
-#if LUA_VERSION_NUM == 501
-        luaL_register(hL, "luadebug", l);
+#if LUA_VERSION_NUM == 501 && !defined(LUAJIT_VERSION)
+#    define luaL_newlibtable(L, l) \
+        lua_createtable(L, 0, sizeof(l) / sizeof((l)[0]) - 1)
+        auto luaL_setfuncs = [](lua_State* L, const luaL_Reg* l, int nup) {
+            luaL_checkstack(L, nup, "too many upvalues");
+            for (; l->name; l++) {
+                int i;
+                for (i = 0; i < nup; i++) /* Copy upvalues to the top. */
+                    lua_pushvalue(L, -nup);
+                lua_pushcclosure(L, l->func, nup);
+                lua_setfield(L, -(nup + 2), l->name);
+            }
+            lua_pop(L, nup); /* Remove upvalues. */
+        };
+
+        luaL_newlibtable(hL, l);
+        luaL_setfuncs(hL, l, 0);
         lua_newuserdata(hL, 0);
 #else
         luaL_newlibtable(hL, l);
