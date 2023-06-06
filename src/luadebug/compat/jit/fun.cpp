8844b228adeb3df86f71ada55436baa59509113e
fesily <fesil@foxmail.com>
2023-06-06 11:21:18 +0800

    fix build err

--- src/luadebug/compat/jit/fun.cpp
+++ src/luadebug/compat/jit/fun.cpp
@@ -2,9 +2,9 @@
 #include <lj_obj.h>
 ProtoInfo lua_getprotoinfo(lua_State* L, Proto* pt) {
     GCstr* name = proto_chunkname(pt);
-    return {
-        .source          = strdata(name),
-        .linedefined     = pt->firstline,
-        .lastlinedefined = (pt->firstline || !pt->numline) ? pt->firstline + pt->numline : 0,
-    };
+    ProtoInfo info;
+    info.source          = strdata(name);
+    info.linedefined     = pt->firstline;
+    info.lastlinedefined = (pt->firstline || !pt->numline) ? pt->firstline + pt->numline : 0;
+    return info;
 }
