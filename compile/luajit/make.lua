cb69ef831298a62c8eaac278a8a1b05e839c2b74
SlawekNowy <sosw.slawomir.spiewak@gmail.com>
2024-05-31 13:37:52 +0200

    Update make.lua
    Add `_XOPEN_SOURCE` to `lj_set_hash`

--- compile/luajit/make.lua
+++ compile/luajit/make.lua
@@ -118,7 +118,8 @@ lm:source_set("lj_str_hash.c") {
         _LARGEFILE_SOURCE,
         LUA_MULTILIB,
         LUAJIT_ENABLE_LUA52COMPAT,
-        LUAJIT_NUMMODE
+        LUAJIT_NUMMODE,
+        "_XOPEN_SOURCE=600"
     },
     flags = lj_str_hash_flags
 }
