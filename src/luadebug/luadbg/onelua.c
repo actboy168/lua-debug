070cf2a64e8c26421a71dba92b1ab2a7d319bfe2
Javier Peletier <jpeletier@users.noreply.github.com>
2025-04-07 03:42:13 +0200

    fix compilation error due to _bee_lua_apicheck() sig mismatch (#316)

--- src/luadebug/luadbg/onelua.c
+++ src/luadebug/luadbg/onelua.c
@@ -94,7 +94,8 @@ void _bee_lua_assert(const char* message, const char* file, unsigned line) {
     abort();
 }
 
-void _bee_lua_apicheck(lua_State* L, const char* message, const char* file, unsigned line) {
+void _bee_lua_apicheck(void* l, const char* message, const char* file, unsigned line) {
+    lua_State* L = l;
     fprintf(stderr, "(%s:%d) %s\n", file, line, message);
     fflush(stderr);
     if (!lua_checkstack(L, LUA_MINSTACK)) {
